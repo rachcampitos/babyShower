6ce05d4a7eaba3e016acd35d3d032d6b7556d030
rachcampitos <raulo_974@hotmail.com>
2020-04-18 22:02:26 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prototype';
+  theme = localStorage.getItem('theme');
+  constructor(public overlayContainer: OverlayContainer) {}
+  lightActive = true;
+  darkActive = false;
+  @HostBinding('class') componentCssClass;
+  ngOnInit() {}
+  changeTheme(theme) {
+    this.theme = theme;
+    console.log(this.theme);
+    this.componentCssClass = theme;
+    if (theme == 'light') {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
+    } else {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+    let isLightTheme: boolean = theme == 'light';
+    this.darkActive = isLightTheme ? true : false;
+    this.lightActive = isLightTheme ? false : true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MatIconModule, MatSlideToggleModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

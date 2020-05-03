e3c2e8a33adb307f423a5416700d95035343d6ee
rachcampitos <raulo_974@hotmail.com>
2020-05-03 12:15:32 -0500

    version 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-
+import * as AOS from 'aos';
 import { OverlayContainer } from '@angular/cdk/overlay';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,7 +16,28 @@ export class AppComponent implements OnInit {
   darkActive = false;
   @HostBinding('class') componentCssClass;
   ngOnInit() {
+    AOS.init();
     document.body.classList.add(this.theme);
+    window.addEventListener('scroll', () => {
+      const header = document.querySelector('header');
+      header.classList.toggle('sticky', window.scrollY > 0);
+    });
+
+    const selectElement = (s) => document.querySelector(s);
+    // Open Menu
+    selectElement('.open').addEventListener('click', () => {
+      selectElement('.nav-list').classList.add('active');
+    });
+    selectElement('.close').addEventListener('click', () => {
+      selectElement('.nav-list').classList.remove('active');
+    });
+    // Top top button
+    const toTop = document.querySelector('.to-top');
+    window.addEventListener('scroll', () => {
+      window.pageYOffset > 100
+        ? toTop.classList.add('enabled')
+        : toTop.classList.remove('enabled');
+    });
     this.changeTheme(this.theme);
   }
   changeTheme(theme) {
